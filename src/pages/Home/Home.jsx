b1f5b600f88b04c58dae6cb95748fb006be93fab
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-12-08 15:49:12 +0600

    feat(HowItWorks): implement HowItWorks section with 3-step grid

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             <Hero/>
             <TuitionPosts/>
             <Tutors/>
-            {/* <HowItWorks/> */}
+            <HowItWorks/>
             <WhyChooseUs/>
         </div>
     );
